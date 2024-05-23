3029aaab94bb8aea33ca283d09e512e0009501fa
Daniela Londono <daniela0393h@gmail.com>
2024-05-22 20:45:31 -0500

    se hace responsive de index y se comienza la ultima pagina

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,15 @@
 import '../scss/styles.scss';
 import * as bootstrap from 'bootstrap'
+
+console.log('Module loaded')
+
+document.addEventListener('DOMContentLoaded', () => {
+    const navbar = document.querySelector('nav')
+    window.addEventListener('scroll', function() {
+        if(window.scrollY > 10) {
+            navbar.classList.add("nav-scrolled-background")
+        } else {
+            navbar.classList.remove("nav-scrolled-background")
+        }
+    })
+})
