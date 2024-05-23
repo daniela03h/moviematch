f2e72255389df4ae98ed26691992d03a892c381e
jarenas1 <juanjoarenas1218gmail.com>
2024-05-23 13:35:19 -0500

    feat/singup/singin/V2Api

--- src/js/api.js
+++ src/js/api.js
@@ -2,49 +2,55 @@ const options = {
   method: 'GET',
   headers: {
     accept: 'application/json',
-    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkNTA4OGJlMTNjMDU5MDY2MjZhZTg5YmI1NTM2MDM2NSIsInN1YiI6IjY2NGIzN2JjMzhkN2FhZDhjOTc5ODY0ZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.I527myp9u6D9tJxiCc1H2dGn_hlqeUMpdBhbYd50Rho'
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxNzkwY2I4YWZlNDVmYzE5OWJkNDBlZDA0OGU2MzBiYSIsInN1YiI6IjY2NGJmYTVjNzNjZGI0NGJiZjMwMTAzNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Fx4EKy7hXNKnVjw6QAIuHSiQ7k-w0YRO9jt4cx3QVJI'
   }
 };
 // calling genres of the api url and transform it to js object
 async function callingGenres() {
+    let genres = []
     const response = await fetch('https://api.themoviedb.org/3/genre/movie/list?language=en', options);
     const data = await response.json();
-    console.log(data);
+    genres.push(data.resultsf)
+    console.log(genres);
   } 
   
 callingGenres();
 
 // ######################################################################################################################################
 
-//find a movie using 3 genres with the id and filter using the age(+18/-18)
-async function callingMoviesByGenres(genres,age){
-  let movies = []
-  let page = 1
-  let maxPage = 2
-  const genresString = genres.join(',')
 
-  while (page<=maxPage) {  //we use page becouse the api returns 1 page witch 20 movies, so we need use more than a page 
-    const response = await fetch(`https://api.themoviedb.org/3/discover/movie?with_genres=${genresString}`, options);
+async function callingMoviesByGenres(genres) {
+  let movies = [];
+  let page = 1;
+  const maxPage = 5; // Define el número máximo de páginas que deseas obtener
+  const genresString = genres.join(',');
+
+  while (page <= maxPage) {
+    const response = await fetch(`https://api.themoviedb.org/3/discover/movie?with_genres=${genresString}&page=${page}`, options);
     const data = await response.json();
-    movies = movies.concat(data.results) //the data is concat because movies is an array, and result is only 1 page in each iteration
-    page++
+      const filteredMovies = data.results.filter(movie => movie.original_language=="en");
+      movies = movies.concat(filteredMovies);
+      page++;
+    }
+    console.log(movies);
   }
-  console.log(movies)
-}
 
-let genres = ['28', '12', '16'] //genres ids
-let age = false //if age == false the movies are family friendly, else are +18
+let genres = ['28', '12', '16']; // IDs de géneros
 
-callingMoviesByGenres(genres,age)
+callingMoviesByGenres(genres);
+
 
 // #######################################################################################################################################
 
 //TREND LIST
 
 async function trendList (){
+  trendMovies = []
   const response = await fetch('https://api.themoviedb.org/3/trending/movie/day?language=en-US', options)
   const data = await response.json();
-  console.log(data.results);  //array with trend list of movies
+  const filteredMovies = data.results.filter(movie => movie.original_language=="en")
+      trendMovies = trendMovies.concat(filteredMovies);
+  console.log(trendMovies);  //array with trend list of movies
 }
 
 trendList()
---
+++ src/js/singin.js
@@ -0,0 +1,41 @@
+import '../scss/singin.scss';
+import * as bootstrap from 'bootstrap'
+
+//call form and inputs 
+
+const form = document.querySelector('form')
+
+const email = document.querySelector('#email')
+
+const password = document.querySelector('#password')
+
+
+form.addEventListener('submit', async(event) => {
+    event.preventDefault()
+    const user = await validateEmail(email)
+    if (user === false){
+        alert("No esta regitrado")
+    }else{
+        if (user.password == password.value){
+
+            localStorage.setItem("userOnline", JSON.stringify(user))  //GUARDAMOS LA SESION DEL USUARIO EN EL LOCAL STORAGE
+
+            window.location.href = "./src/pages/index.html"
+        }else{
+            alert("Contraseña Incorrecta")
+        }
+    }
+})
+
+
+async function validateEmail(email) {
+    const response = await fetch(`http://localhost:3000/users?email=${email.value}`) // añadimos ?email=${email} PARA PASARLE PARAMETROS A LA URL
+    const data = await response.json()
+ 
+    if (data.length>0) {
+         return data[0]          //TRAEMOS EL OBJETO QUE NOS VA A TRAER DATA (que por logica solo va a existir 1, asi que pedimos posicion 0)  //VALIDAMOS SI EXISTE UN USUARIO CON EL EMAIL O NO 
+    }else{
+         return false
+    }
+ }
+ 
