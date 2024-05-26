d11c66a5be5b580ba557075ee12e2a94db15c1e6
jarenas1 <juanjoarenas1218gmail.com>
2024-05-26 12:33:43 -0500

    rematchworking

--- src/js/api.js
+++ src/js/api.js
@@ -1,56 +1,126 @@
-const options = {
+export const options = {
   method: 'GET',
   headers: {
     accept: 'application/json',
     Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxNzkwY2I4YWZlNDVmYzE5OWJkNDBlZDA0OGU2MzBiYSIsInN1YiI6IjY2NGJmYTVjNzNjZGI0NGJiZjMwMTAzNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Fx4EKy7hXNKnVjw6QAIuHSiQ7k-w0YRO9jt4cx3QVJI'
   }
 };
+
 // calling genres of the api url and transform it to js object
-async function callingGenres() {
-    let genres = []
+export async function callingGenres(site) {
     const response = await fetch('https://api.themoviedb.org/3/genre/movie/list?language=en', options);
     const data = await response.json();
-    genres.push(data.resultsf)
-    console.log(genres);
+    data.genres.forEach((genre) => { //iterar el objeto "genres" del objeto "data" (esto ya que el array contiene dos elementos)
+
+      //creación de cada boton asignandoseles su id y su nombre
+      site.innerHTML += `
+      <button type="button" class="genre-btn" id="${genre.id}" name="${genre.name}">
+      ${genre.name}
+      </button>`;
+    });
+
   } 
   
-callingGenres();
+
 
 // ######################################################################################################################################
 
 
-async function callingMoviesByGenres(genres) {
+
+export  async function callingMoviesByGenres(genres,sitio,movieMatched) {
+  let url = "https://image.tmdb.org/t/p/w500/"
   let movies = [];
   let page = 1;
-  const maxPage = 5; // Define el número máximo de páginas que deseas obtener
-  const genresString = genres.join(',');
-
+  let ids = []
+  const maxPage = 8; // MAX PAGES TO GET MOVIES(20 MOVIES PER PAGE)
   while (page <= maxPage) {
-    const response = await fetch(`https://api.themoviedb.org/3/discover/movie?with_genres=${genresString}&page=${page}`, options);
+    const response = await fetch(`https://api.themoviedb.org/3/discover/movie?with_genres=${genres}&page=${page}`, options);
     const data = await response.json();
-      const filteredMovies = data.results.filter(movie => movie.original_language=="en");
+      const filteredMovies = data.results.filter(movie => movie.original_language=="en"&&movie.backdrop_path !== null);
       movies = movies.concat(filteredMovies);
       page++;
     }
-    console.log(movies);
+    localStorage.setItem("movies",  JSON.stringify(movies));
+    const movie = movies[movieMatched]
+    localStorage.setItem("movies",(movie.title,url+movie.backdrop_path,movie.overview,movie.id,url+movie.poster_path));
+    ids = ids.concat(movie.id);
+    localStorage.setItem("ids",ids)
+    sitio.innerHTML = `
+    <div class="col-md-3 col-sm-6 col-12">
+    <div class="card">
+      <img src="${url+movie.backdrop_path}" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">${movie.title}</h5>
+        <p class="card-text">${movie.overview}</p>
+        <p class="card-text">${movie.overview}</p>
+        <p class="card-text">${movie.overview}</p>
+      </div>
+      <div class="card-footer">
+        <small class="text-muted">ID: ${movie.id}</small>
+      </div>
+    </div>
+    </div>
+    `; 
   }
 
-let genres = ['28', '12', '16']; // IDs de géneros
-
-callingMoviesByGenres(genres);
 
 
 // #######################################################################################################################################
 
 //TREND LIST
 
-async function trendList (){
-  trendMovies = []
-  const response = await fetch('https://api.themoviedb.org/3/trending/movie/day?language=en-US', options)
+// async function trendList (){
+//   let trendMovies = []
+//   const response = await fetch('https://api.themoviedb.org/3/trending/movie/day?language=en-US', options)
+//   const data = await response.json();
+//   const filteredMovies = data.results.filter(movie => movie.original_language=="en")
+//       trendMovies = trendMovies.concat(filteredMovies);
+//   // console.log(trendMovies);  //array with trend list of movies
+  
+// }
+
+// trendList()
+
+
+//MOVIE PROVIDERS IMAGE
+
+ export async function getProviders(id) {
+  const response = await fetch(`https://api.themoviedb.org/3/movie/${id}/watch/providers?watch_region=CO`, options)
   const data = await response.json();
-  const filteredMovies = data.results.filter(movie => movie.original_language=="en")
-      trendMovies = trendMovies.concat(filteredMovies);
-  console.log(trendMovies);  //array with trend list of movies
+  // console.log(data.results.CO.rent);
+  let url = "https://image.tmdb.org/t/p/w500/"
+  let providers = await data.results.CO?.rent 
+  await providers?.forEach(plataform => {
+    if(providers !== null){
+      console.log(id, ":",url+plataform.logo_path)
+    }else{
+      console.log(id,"no hayy");
+    }
+    //RETURN URL IMAGE TO PROVIDERS
+  });
 }
 
-trendList()
+// getProviders(823464)
+
+//MATCH MOVIE //ASIGN SELECTED MOVIE INTO USER WITH KEY SELECTEDMOVIE
+
+// async function selectedMovie (){
+//    function registerUser (user,lastName,email,password) {
+    
+//     const  newUser = {
+//          username: user.value.toLowerCase(),
+//          lastName: lastName.value.toLowerCase(),
+//          email: email.value,
+//          password:  password.value
+//      }
+ 
+//      await fetch (`http://localhost:3000/users`,{
+//      method: 'POST',
+//      headers: {
+//          'Content-Type': 'application/json'
+//      },
+//      body: JSON.stringify(newUser)    //EN BODY COLOCAMOS EL JSON QUE VAMOS A GUARDAR EN LA DB (TAMBIEN LO CONVERTIMOS A JSON)
+//  })
+ 
+//  }
+// }
---
+++ src/js/genreSelector.js
@@ -0,0 +1,83 @@
+
+import "../scss/genreSelector.scss";
+import * as bootstrap from "bootstrap";
+import {options} from "./api"
+import{callingGenres} from "./api"
+import { callingMoviesByGenres } from "./api";
+
+let genresSection = document.getElementById("genres");
+let genresSelected = document.getElementById("selected-genres");
+let moviesec = document.getElementById("pedrojuan");
+let selectedGenre = {};
+let genres=[];
+
+const botonSearch = document.getElementById("search");
+
+console.log(moviesec);
+
+window.addEventListener("load", () => {
+    localStorage.clear()
+})
+
+// Llamado a la API recomendado por TMDB (la API que se está utilizando)
+
+callingGenres(genresSection)
+
+
+// Evento de creación de géneros seleccionados
+genresSection.addEventListener("click", (event) => {
+  let id = event.target.getAttribute("id");
+  let name = event.target.getAttribute("name");
+  genreSel(id, name); // Añadir género al array "selectedGenre"
+  showSelectedGenres(id, name); // Mostrar los géneros de este array en su contenedor
+});
+
+// Evento de eliminación de géneros seleccionados
+genresSelected.addEventListener("click", (event) => {
+  let id = event.target.getAttribute("id");
+  deleteGenre(id);
+});
+
+// Añadidor de géneros al array
+function genreSel(idGenreSelected, nameGenreSelected) {
+  selectedGenre[idGenreSelected] = nameGenreSelected;
+  console.log(selectedGenre); // Prueba 
+}
+
+// Mostrar los géneros seleccionados
+function showSelectedGenres() {
+  genresSelected.innerHTML = ""; // Limpiar contenedor de los géneros seleccionados
+  let genresCounter = 0; // Contador de los géneros que han sido seleccionados
+  for (const idGenreSelected of Object.keys(selectedGenre)) { // Iteración del objeto "selectedGenre" que imprime en el contenedor "genresSelected" cada género seleccionado
+    if (genresCounter < 3) { // Cantidad máxima de géneros admitible
+      genresSelected.innerHTML += ` 
+      <button type="button" class="genre-btn" id="${idGenreSelected}" name="${selectedGenre[idGenreSelected]}">${selectedGenre[idGenreSelected]}</button>`; // Creación de botones con cada género seleccionado
+      genresCounter++;
+    } else {
+      alert("el máximo de géneros agregable es 3"); // Alerta en caso de que se supere la capacidad máxima
+      delete selectedGenre[idGenreSelected]; // Eliminar género sobrante del objeto "selectedGenre"
+    }
+  }
+}
+
+// Eliminar los géneros seleccionados
+function deleteGenre(idGenreSelected) {
+  delete selectedGenre[idGenreSelected];
+  showSelectedGenres();
+}
+
+// Mandar el id de los géneros al array "genres"
+botonSearch.addEventListener("click", function() {
+  // Array de géneros seleccionados
+  let genresA = Object.keys(selectedGenre); // Creación del array "genres"
+  console.log("GÉNEROS SELECCIONADOS:", genresA); 
+  genres.push(...genresA)
+  const genresString = genres.join(',')
+    console.log(genres);
+    window.location.href = "../pages/hola.html"
+//   callingMoviesByGenres(genresString);
+  localStorage.setItem("genres", genresString)
+
+});
+
+
